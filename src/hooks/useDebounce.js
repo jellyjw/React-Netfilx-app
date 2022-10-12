02866049d32b1aa0e84187555dd6c028956c926c
jellyjw <wldnzzz@naver.com>
2022-10-12 15:29:14 +0900

    modal pages

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-//리액트에서 hooks를 만들때에는 항상 파일명이 use로 시작해야한다.
-=======
->>>>>>> 7a12e90c891637d6914ebde6a2d9c32fe623e682
 import { useState, useEffect } from "react";
 
 export const useDebounce = (value, delay) => {
