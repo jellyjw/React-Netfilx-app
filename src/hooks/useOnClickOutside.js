3e92c15b01e68a7fe14554e3b5deeb42cc06fa23
jellyjw <wldnzzz@naver.com>
2022-10-12 15:26:54 +0900

    modal pages

--- src/hooks/useOnClickOutside.js
+++ src/hooks/useOnClickOutside.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
 
 const useOnClickOutside = (ref, handler) => {
@@ -21,27 +20,3 @@ const useOnClickOutside = (ref, handler) => {
 };
 
 export default useOnClickOutside;
-=======
-import { useEffect } from 'react';
-
-export default function useOnClickOutside(ref, handler) {
-    
-    useEffect(
-        () => {
-            const listener = (event) => {
-                if (!ref.current || ref.current.contains(event.target)) {
-                    return;
-                }
-                handler(event);
-            };
-            document.addEventListener("mousedown", listener);
-            document.addEventListener("touchstart", listener);
-            return () => {
-                document.removeEventListener("mousedown", listener);
-                document.removeEventListener("touchstart", listener);
-            };
-        },
-        [ref, handler]
-    );
-}
->>>>>>> 7a12e90c891637d6914ebde6a2d9c32fe623e682
