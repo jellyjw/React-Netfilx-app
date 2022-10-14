db6e33e0a9f90655612ff4ce6c05d19b873cf502
jellyjw <wldnzzz@naver.com>
2022-10-14 14:26:13 +0900

    react-netfilx-app

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "0b1a4677ca2ebb0fa60cfebc75c05f5d",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 });
